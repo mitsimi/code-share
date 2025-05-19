7b551a1ae07287a90c3e25b163bcd5a43af2b6d2
mitsimi <noah.fuereder@gmail.com>
2025-05-19 21:31:27 +0200

    change width to better fit longer lines

--- frontend/src/views/SnippetDetailsView.vue
+++ frontend/src/views/SnippetDetailsView.vue
@@ -56,7 +56,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="mx-auto my-12 max-w-4xl px-4">
+  <main class="mx-auto my-12 w-fit max-w-7xl min-w-4xl px-4">
     <!-- Back button -->
     <Button variant="outline" @click="router.back()">
       <ArrowLeft class="size-5" />
