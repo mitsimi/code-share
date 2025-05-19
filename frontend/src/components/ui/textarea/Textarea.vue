757ca47bcdd706fc1b87b9ff8271c6a0c97a7ffa
mitsimi <noah.fuereder@gmail.com>
2025-05-19 21:56:14 +0200

    background for textarea

--- frontend/src/components/ui/textarea/Textarea.vue
+++ frontend/src/components/ui/textarea/Textarea.vue
@@ -27,7 +27,7 @@ const modelValue = useVModel(props, 'modelValue', emits, {
     :rows="rows"
     :class="
       cn(
-        'focus:ring-accent placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex w-full rounded-lg border-4 border-black bg-transparent px-3 py-2 font-mono text-base shadow-[4px_4px_0_0_#000] transition-[color,box-shadow] outline-none focus:ring-2 focus:outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'focus:ring-accent placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 bg-background flex w-full rounded-lg border-4 border-black px-3 py-2 font-mono text-base shadow-[4px_4px_0_0_#000] transition-[color,box-shadow] outline-none focus:ring-2 focus:outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         props.class,
       )
     "
