49957c51034373fa896d6a996a3873598dbbf311
mitsimi <noah.fuereder@gmail.com>
2025-05-24 23:22:07 +0200

    remove sidebar customization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,27 +19,6 @@ import AppFooter from './components/AppFooter.vue'
 </script>
 
 <style>
-/* Custom scrollbar styling */
-::-webkit-scrollbar {
-  width: 16px;
-  height: 16px;
-}
-
-::-webkit-scrollbar-track {
-  background: #fcf7de;
-  border-left: 3px solid #000000;
-}
-
-::-webkit-scrollbar-thumb {
-  background: var(--destructive);
-  border: 3px solid #000000;
-  box-shadow: inset 2px 2px 0 rgba(255, 255, 255, 0.3);
-}
-
-::-webkit-scrollbar-corner {
-  background: #fcf7de;
-}
-
 /* Sonner toast customization */
 :root {
   --toast-bg: #fcf7de;
