2fae106c3be9e9c3eaadbe6eceb88e63d7dd7a26
mitsimi <noah.fuereder@gmail.com>
2025-05-28 17:10:52 +0200

    change load icon and improve code structure

--- frontend/src/components/LikeButton.vue
+++ frontend/src/components/LikeButton.vue
@@ -5,34 +5,17 @@
     :class="{ 'pointer-events-none': !authStore.isAuthenticated() }"
   >
     <span>{{ likes }}</span>
-    <Heart v-if="!isLoading" class="size-5" :fill="is_liked ? 'red' : 'none'" />
-    <div v-else class="flex items-center gap-2">
-      <svg
-        class="text-primary h-5 w-5 animate-spin"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <template v-if="isLoading">
+      <LoaderCircleIcon class="animate-spin" />
+    </template>
+    <template v-else>
+      <Heart class="size-5" :fill="is_liked ? 'red' : 'none'" />
+    </template>
   </Button>
 </template>
 
 <script setup lang="ts">
-import { Heart } from 'lucide-vue-next'
+import { Heart, LoaderCircleIcon } from 'lucide-vue-next'
 import { Button } from './ui/button'
 import { type Snippet } from '@/types'
 import { ref } from 'vue'
