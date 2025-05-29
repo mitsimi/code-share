09653cef4c7668596fd0772dd72f2dfc5dd0898e
mitsimi <noah.fuereder@gmail.com>
2025-05-29 16:14:16 +0200

    remove old header and active color for links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-background flex min-h-screen flex-col">
-    <AppHeader_v2 />
+    <AppHeader />
     <main class="grow">
       <RouterView />
     </main>
@@ -15,7 +15,7 @@ import { RouterView } from 'vue-router'
 import { Toaster } from 'vue-sonner'
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 import AppFooter from './components/AppFooter.vue'
-import AppHeader_v2 from './components/AppHeader_v2.vue'
+import AppHeader from './components/AppHeader.vue'
 </script>
 
 <style>
