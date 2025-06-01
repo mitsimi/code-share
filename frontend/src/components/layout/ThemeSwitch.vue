ea0b09fe885e88a285a8a50935a0e8900a187cd7
mitsimi <noah.fuereder@gmail.com>
2025-06-02 00:13:37 +0200

    use useColorMode from @vueuse/core
    fixes #2

--- frontend/src/components/layout/ThemeSwitch.vue
+++ frontend/src/components/layout/ThemeSwitch.vue
@@ -5,14 +5,19 @@
         <span class="sr-only">Toggle Theme</span>
         <div class="flex flex-row items-center justify-center">
           <div class="">
-            <span v-if="themeStore.theme === 'light'" class="theme-icon">
+            <!-- Display icon based on the selected mode (light, dark, or auto/system) -->
+            <span v-if="store === 'light'" class="theme-icon">
               <SunIcon class="size-4" />
             </span>
-            <span v-else-if="themeStore.theme === 'dark'" class="theme-icon">
+            <span v-else-if="store === 'dark'" class="theme-icon">
               <MoonIcon class="size-4" />
             </span>
-            <span v-else>
+            <span v-else-if="store === 'auto'">
+              <!-- 'auto' corresponds to 'system' -->
               <SunMoonIcon class="size-4" />
+            </span>
+            <span v-else>
+              <CircleHelpIcon class="size-4" />
             </span>
           </div>
         </div>
@@ -23,27 +28,27 @@
       <ContextMenuGroup>
         <ContextMenuItem
           value="light"
-          @click="themeStore.changeTheme('light')"
+          @click="store = 'light'"
           class="flex items-center justify-between"
         >
           <div class="flex items-center gap-2"><SunIcon /> Light</div>
-          <CheckIcon v-if="themeStore.theme === 'light'" class="size-4" />
+          <CheckIcon v-if="store === 'light'" class="size-4" />
         </ContextMenuItem>
         <ContextMenuItem
           value="dark"
-          @click="themeStore.changeTheme('dark')"
+          @click="store = 'dark'"
           class="flex items-center justify-between"
         >
           <div class="flex items-center gap-2"><MoonIcon /> Dark</div>
-          <CheckIcon v-if="themeStore.theme === 'dark'" class="size-4" />
+          <CheckIcon v-if="store === 'dark'" class="size-4" />
         </ContextMenuItem>
         <ContextMenuItem
           value="system"
-          @click="themeStore.changeTheme('system')"
+          @click="store = 'auto'"
           class="flex items-center justify-between"
         >
           <div class="flex items-center gap-2"><SunMoonIcon /> System</div>
-          <CheckIcon v-if="themeStore.theme === 'system'" class="size-4" />
+          <CheckIcon v-if="store === 'auto'" class="size-4" />
         </ContextMenuItem>
       </ContextMenuGroup>
     </ContextMenuContent>
@@ -51,14 +56,27 @@
 </template>
 
 <script lang="ts" setup>
-import { useThemeStore } from '@/stores/theme'
-import { SunIcon, MoonIcon, SunMoonIcon } from 'lucide-vue-next'
-import { CheckIcon } from 'lucide-vue-next'
+import { CheckIcon, SunIcon, MoonIcon, SunMoonIcon, CircleHelpIcon } from 'lucide-vue-next'
+import { useColorMode } from '@vueuse/core'
 
-const themeStore = useThemeStore()
+const { system, store } = useColorMode({
+  storageKey: 'theme',
+  selector: 'html',
+  attribute: 'class',
+  initialValue: 'auto',
+  modes: {
+    dark: 'dark',
+    light: '',
+  },
+})
 
 const toggleTheme = () => {
-  const newTheme = themeStore.resolvedTheme === 'light' ? 'dark' : 'light'
-  themeStore.changeTheme(newTheme)
+  if (store.value === 'auto' && system.value === 'light') {
+    store.value = 'dark'
+  } else if (store.value === 'auto' && system.value === 'dark') {
+    store.value = 'light'
+  } else {
+    store.value = store.value === 'light' ? 'dark' : 'light'
+  }
 }
 </script>
