6cbbb6737e83a5a3f04004795b9387be6e48729d
mitsimi <noah.fuereder@gmail.com>
2025-06-17 15:00:36 +0200

    extract user menu for desktop and mobile

---
+++ frontend/src/components/UserMenu/UserInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <Avatar class="size-8">
+      <AvatarImage :src="authStore.user?.avatar" />
+      <AvatarFallback>{{ authStore.user?.username[0].toUpperCase() }}</AvatarFallback>
+    </Avatar>
+    <div class="flex flex-col">
+      <span class="text-sm font-medium">{{ authStore.user?.username }}</span>
+      <span class="text-muted-foreground text-xs">{{ authStore.user?.email }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+
+const authStore = useAuthStore()
+</script>
