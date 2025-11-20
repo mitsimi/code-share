ec9b3b333e280f4316bb8956d5a609bbdd6abd34
mitsimi <noah.fuereder@gmail.com>
2025-11-20 17:24:59 +0100

    fix(ui): add line-clamp property to SnippetCard component for compatibility

--- frontend/src/components/snippets/SnippetCard.vue
+++ frontend/src/components/snippets/SnippetCard.vue
@@ -126,6 +126,7 @@ const copyToClipboard = async () => {
 .line-clamp-2 {
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
